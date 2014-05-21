8320e9afb86aa4916a9c25574dfb15d9b1f59041
Andrey Vasilenkov <nobody.eie@gmail.com>
2014-05-21 19:06:03 +0400

    Updated json response class and added new handlers

--- src/cocaine-app/flowmastermind/__init__.py
+++ src/cocaine-app/flowmastermind/__init__.py
@@ -1,4 +1,5 @@
 import datetime
+from functools import wraps
 import json
 import traceback
 
@@ -6,7 +7,7 @@ import msgpack
 
 from cocaine.services import Service
 from cocaine.logging import Logger
-from flask import Flask, Response
+from flask import Flask, Response, request
 from flask import abort, render_template
 
 from flowmastermind.test import ping
@@ -15,6 +16,37 @@ from flowmastermind.test import ping
 logging = Logger()
 
 app = Flask(__name__)
+
+
+def json_response(func):
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            res = {'status': 'success',
+                   'response': func(*args, **kwargs)}
+        except Exception as e:
+            logging.error(e)
+            logging.error(traceback.format_exc())
+            res = {'status': 'error',
+                   'error_message': str(e)}
+
+        return JsonResponse(json.dumps(res))
+
+    return wrapper
+
+
+class JsonResponse(Response):
+    def __init__(self, *args, **kwargs):
+        super(JsonResponse, self).__init__(*args, **kwargs)
+        self.headers['Cache-Control'] = 'no-cache, must-revalidate'
+        self.headers['Content-Type'] = 'application/json'
+
+
+def mastermind_response(response):
+    if isinstance(response, dict) and 'Balancer error' in response:
+        raise RuntimeError(response['Balancer error'])
+    return response
 
 
 @app.route('/')
@@ -124,12 +156,52 @@ def json_group_info(group_id):
         raise
 
 
-class JsonResponse(Response):
-    def __init__(self, *args, **kwargs):
-        super(JsonResponse, self).__init__(*args, **kwargs)
-        logging.info(self)
-        self.headers['Cache-Control'] = 'no-cache, must-revalidate'
-        self.headers['Content-Type'] = 'application/json'
+@app.route('/json/commands/status/<uid>/')
+@json_response
+def json_command_status(uid):
+    m = Service('mastermind')
+    status = mastermind_response(m.enqueue('get_command',
+        msgpack.packb([uid.encode('utf-8')])).get())
+
+    return status
+
+
+@app.route('/json/commands/execute/node/shutdown/', methods=['POST'])
+@json_response
+def json_commands_node_shutdown():
+    node = request.form.get('node')
+    host, port = node.split(':')
+    if not node:
+        raise ValueError('Node should be specified')
+    m = Service('mastermind')
+    cmd = mastermind_response(m.enqueue('shutdown_node_cmd',
+        msgpack.packb([node.encode('utf-8')])).get())
+
+    resp = mastermind_response(m.enqueue('execute_cmd',
+        msgpack.packb([host, cmd, {'node': node}])).get())
+
+    uid = resp.keys()[0]
+
+    return uid
+
+
+@app.route('/json/commands/execute/node/start/', methods=['POST'])
+@json_response
+def json_commands_node_start():
+    node = request.form.get('node')
+    host, port = node.split(':')
+    if not node:
+        raise ValueError('Node should be specified')
+    m = Service('mastermind')
+    cmd = mastermind_response(m.enqueue('start_node_cmd',
+        msgpack.packb([node.encode('utf-8')])).get())
+
+    resp = mastermind_response(m.enqueue('execute_cmd',
+        msgpack.packb([host, cmd, {'node': node}])).get())
+
+    uid = resp.keys()[0]
+
+    return uid
 
 
 if __name__ == '__main__':
