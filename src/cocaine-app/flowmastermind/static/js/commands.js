039b526af65a9e8260bf106c84c65afdc271b209
Andrey Vasilenkov <nobody.eie@gmail.com>
2014-10-21 16:35:30 +0400

    Increased commands timeout

--- src/cocaine-app/flowmastermind/static/js/commands.js
+++ src/cocaine-app/flowmastermind/static/js/commands.js
@@ -1,13 +1,13 @@
 (function() {
 
-    var updatePeriod = 5000,
+    var updatePeriod = 10000,
         commands = Commands($('.cmd-containers'));
 
     function updateStats() {
         $.ajax({
             url: '/json/commands/',
             data: {ts: new Date().getTime()},
-            timeout: 3000,
+            timeout: 30000,
             dataType: 'json',
             success: function (data) {
 
