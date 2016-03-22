732b9830d1b9b71a48be64c34118b13467bc6c56
Andrey Vasilenkov <indigo@yandex-team.ru>
2016-03-22 18:06:53 +0300

    Change profile name

--- src/cocaine-app/upload.sh
+++ src/cocaine-app/upload.sh
@@ -1,4 +1,4 @@
 find -L . -type f -regex '.*\.\(py\|js\|json\|css\|png\|gif\|html\)'  -print0 | tar czvf ../package.tar.gz --null -T - &&\
     cocaine-tool app upload --package ../package.tar.gz -n flowmastermind --manifest flowmastermind.manifest &&
-    cocaine-tool app restart -n flowmastermind -r default &&
+    cocaine-tool app restart -n flowmastermind -r flowmastermind &&
     rm ../package.tar.gz
