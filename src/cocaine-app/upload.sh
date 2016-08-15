49734fbd604c2e88417d49c2f25c3ab9da7399ae
Andrey Vasilenkov <indigo@yandex-team.ru>
2016-08-15 17:15:12 +0300

    Update flowmastermind profile on dev upload

--- src/cocaine-app/upload.sh
+++ src/cocaine-app/upload.sh
@@ -1,4 +1,5 @@
 find -L . -type f -regex '.*\.\(py\|js\|json\|css\|png\|gif\|html\)'  -print0 | tar czvf ../package.tar.gz --null -T - &&\
     cocaine-tool app upload --package ../package.tar.gz -n flowmastermind --manifest flowmastermind.manifest &&
+    cocaine-tool profile upload -n flowmastermind --profile flowmastermind.profile &&
     cocaine-tool app restart -n flowmastermind -r flowmastermind &&
     rm ../package.tar.gz
