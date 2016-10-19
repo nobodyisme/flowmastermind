9e830f5757b96d1c972d62670be685e00db566a9
Andrey Vasilenkov <indigo@yandex-team.ru>
2016-10-19 12:14:15 +0300

    Update deploy script for the application

--- usr/bin/flowmastermind_deploy.sh
+++ usr/bin/flowmastermind_deploy.sh
@@ -7,6 +7,7 @@ echo "Clean old version of Flowmastermind:"
 for app in 'flowmastermind';
 do 
     rm -rf /var/lib/cocaine/apps/$app
+    rm -f /var/lib/cocaine/manifests/$app
     rm -rf /var/spool/cocaine/$app
     rm -rf /var/cache/cocaine/apps/$app
     rm -f /var/cache/cocaine/manifests/$app
@@ -16,7 +17,3 @@ echo "Deploy New Flowmastermind:"
 cocaine-tool app upload --manifest $DEPLOY_DIR/cocaine-app/flowmastermind.manifest --package $DEPLOY_DIR/cocaine-app/flowmastermind.tar.gz -n flowmastermind
 cocaine-tool profile upload -n flowmastermind --profile $DEPLOY_DIR/cocaine-app/flowmastermind.profile
 cocaine-tool runlist add-app -n default --app flowmastermind --profile flowmastermind --force
-
-chown cocaine -R /usr/lib/flowmastermind
-
-/etc/init.d/cocaine-runtime restart
