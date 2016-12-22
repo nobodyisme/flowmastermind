ee40c96d23231061b94c3ae39325e4a4ffc7c4b9
Andrey Vasilenkov <indigo@yandex-team.ru>
2016-12-23 00:51:40 +0300

    Add external storage task render

--- src/cocaine-app/flowmastermind/static/js/jobs_view.js
+++ src/cocaine-app/flowmastermind/static/js/jobs_view.js
@@ -501,6 +501,7 @@ var Jobs = (function () {
             task_state['type'] == 'node_stop_task' ||
             task_state['type'] == 'node_backend_defrag_task' ||
             task_state['type'] == 'recover_dc_group_task' ||
+            task_state['type'] == 'external_storage' ||
             task_state['type'] == 'rsync_backend_task') {
             this.renderMinionCmdFields(task_state, task_maintitle, task_subtitle, task_additional_data);
         } else if (task_state['type'] == 'history_remove_node') {
