c25eeb1fa866e94ed688f830297e102d5088695f
Andrey Vasilenkov <indigo@yandex-team.ru>
2017-08-10 12:34:11 +0300

    Render lrc-tools tasks

--- src/cocaine-app/flowmastermind/static/js/jobs_view.js
+++ src/cocaine-app/flowmastermind/static/js/jobs_view.js
@@ -651,7 +651,9 @@ var Jobs = (function () {
             task_state['type'] == 'recover_dc_group_task' ||
             task_state['type'] == 'external_storage' ||
             task_state['type'] == 'lrc_recovery' ||
-            task_state['type'] == 'rsync_backend_task') {
+            task_state['type'] == 'rsync_backend_task' ||
+            task_state['type'] == 'lrc_tools'
+        ) {
             this.renderMinionCmdFields(task_state, task_maintitle, task_subtitle, task_additional_data);
         } else if (task_state['type'] == 'history_remove_node') {
             this.renderHistoryRemoveNodeFields(task_state, task_maintitle, task_subtitle, task_additional_data);
