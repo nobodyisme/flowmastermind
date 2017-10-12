a7de695b123fd0da98c539477c02c6ba43dd275c
Andrey Vasilenkov <indigo@yandex-team.ru>
2017-10-13 00:22:50 +0300

    Fix for new templates

--- make_tree.sh
+++ make_tree.sh
@@ -4,7 +4,12 @@ CURDIR=$PWD
 
 #---------- mastermind code
 cd $1/src/cocaine-app/
-tar cvf $CURDIR/debian/tmp/usr/lib/flowmastermind/cocaine-app/flowmastermind.tar.gz *.py flowmastermind/*.py flowmastermind/templates/*.html flowmastermind/static
+tar cvf $CURDIR/debian/tmp/usr/lib/flowmastermind/cocaine-app/flowmastermind.tar.gz \
+    *.py \
+    flowmastermind/*.py \
+    flowmastermind/templates/*.html \
+    flowmastermind/templates/utils/*.html \
+    flowmastermind/static
 cd $CURDIR
 cp $1/src/cocaine-app/*manifest $CURDIR/debian/tmp/usr/lib/flowmastermind/cocaine-app/
 cp $1/src/cocaine-app/flowmastermind.profile $CURDIR/debian/tmp/usr/lib/flowmastermind/cocaine-app/
